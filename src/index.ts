e0f29f6308d64067c0fd53180b5dc14da3e4dad5
Sebastijan Kelnerič <sebastijan.kelneric@sebba.dev>
2024-02-01 03:19:37 +0100

    Remove unnecessary exports from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,1 @@
 export * from "./magnolia";
-export * as node from "./ui/node";
-export * as node_type from "./ui/node_type";
-export * as router from "./router";
-export * as view from "./ui/view";
-export * as state from "./state"
