020ddc684b5e22779f342c2c1a3d97ac90f73b3b
Sebastijan Kelnerič <sebastijan.kelneric@sebba.dev>
2024-02-13 16:38:14 +0100

    Add debug log in Magnolia initialization

--- src/magnolia.ts
+++ src/magnolia.ts
@@ -43,6 +43,8 @@ export class Magnolia {
 
         magnolia = this;
 
+        console.debug("Magnolia#init() called");
+
         // Get the current path
         const base: string = window.location.protocol + "//" + window.location.host;
         const path: string = window.location.href.replace(base, '');
